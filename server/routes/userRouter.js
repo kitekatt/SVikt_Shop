7fec2055e76015872336c42232b158a5c3181385
zero <kat.rinasmn@gmail.com>
2024-03-28 18:39:48 +0300

    first

---
+++ server/routes/userRouter.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = new express.Router()
+const userController = require('../controllers/userController')
+const authMiddleware = require('../middleware/authMiddleWare')
+const adminMiddleware = require('../middleware/adminMiddleware')
+
+router.post('/signup', userController.signup)
+router.post('/login', userController.login)
+router.get('/check', authMiddleware, userController.check)
+
+//ТОЛЬКО ДЛЯ АДМИНИСТРАТОРА
+router.get('/getall',
+ authMiddleware, adminMiddleware, 
+ userController.getAll)
+
+router.get('/getone/:id([0-9]+)', 
+authMiddleware, adminMiddleware, 
+userController.getOne)
+
+router.post('/create',
+ authMiddleware, adminMiddleware, 
+ userController.create)
+
+// router.delete('/delete/:id([0-9]+)', 
+// authMiddleware, 
+// adminMiddleware, userController.delete)
+
+module.exports = router
